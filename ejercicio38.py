6a8d03195cb5e9fa86389fd0cbb3d34eaf021dff
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 09:59:52 -0500

    Add files via upload

---
+++ ejercicio38.py
@@ -0,0 +1,6 @@
+numero1  =  int ( input ( "ingrese el primer número:" ))
+numero2  =  int ( input ( "ingrese el segundo número:" ))
+si  0  <=  numero1  <  6  y  0  <=  numero2  <  6 :
+    imprimir ( "Verdadero" )
+otra cosa :
+    imprimir ( "falso" )
